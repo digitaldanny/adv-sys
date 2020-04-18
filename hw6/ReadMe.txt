7e1b338437d61551bab17429bc2d7c113b09a985
Daniel Hamilton <d.hamilton@ufl.edu>
2020-04-18 09:36:23 -0400

    deadlock case 3

--- hw6/ReadMe.txt
+++ hw6/ReadMe.txt
@@ -36,7 +36,13 @@ will infinite loop after outputting "Open 2."
 second thread to open the device also. The second thread sets to MODE 1, which causes a deadlock for
 thread 1 operation.
 
-- 3.) 
+- 3.) Create two threads. The first thread opens the device. The second thread opens the device but is blocked
+indefinitely while waiting for sem2. The first thread tries to switch to mode 2 by using ioctl(MODE_2) but is
+block indefinitely while waiting on queue 1. 
+
+- 4.) Create two threads. The first thread opens the device and switches into mode 2 using ioctl(MODE_2). The 
+second thread opens the device successfully. Both threads try to switch to mode 1 using ioctl(MODE_1) but block
+indefinitely while waiting on queue 2.
 
 ##############################################################
 Data Race Cases
@@ -44,8 +50,10 @@ Data Race Cases
 
 - 1.) Thread 1 opens device with global file descriptor (fd). Thread 1 starts performing reads / writes. Thread 2
 does not have to open the device because it is already opened with the global fd, so thread 2 starts performing 
-reads / writes without opening a new file descriptor. This is a data race for the device buffer.
+reads / writes without opening a new file descriptor. This is a data race for the device buffer. The critical region
+is between thread 1's open and thread 1's close.
 
 - 2.) This case is similar to the first data race case, except with multi-process usage. The parent process opens
 the device with file descriptor (fd). Then the process forks and the child process can access the device without
-opening the device using fd. Again, this is a data race for the device buffer.
+opening the device using fd. Again, this is a data race for the device buffer. The critical region is between
+parent process open and parent process close.
--- hw6/userapp3.c
+++ hw6/userapp3.c
@@ -6,14 +6,111 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <pthread.h>
 
 #define DEVICE "/dev/a5"
 
 #define CDRV_IOC_MAGIC 'Z'
-#define ASP_CLEAR_BUF _IOW(CDRV_IOC_MAGIC, 1, int)
+#define E2_IOCMODE1 _IOWR(CDRV_IOC_MAGIC, 1, int)
+#define E2_IOCMODE2 _IOWR(CDRV_IOC_MAGIC, 2, int)
 
+/*
+ * +=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+
+ *                                PROTOTYPES
+ * +=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+
+*/
+void *thread1();
+void *thread2();
 
-int main()
+/*
+ * +=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+
+ *                                  GLOBALS
+ * +=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+
+*/
+pthread_t threadid[2];
+
+/*
+ * +=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+
+ *                             THREAD DEFINITIONS
+ * +=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+
+*/
+
+/*
+ * +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+ * SUMMARY: main
+ * This thread launches threads 1 and 2 then waits for them to join. For this
+ * demo, the final message should not display. This will show that the 
+ * program is in a deadlock.
+ * +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+*/
+int main() 
 {
-    return 0;
+    printf("Device %s\n", DEVICE);
+
+    pthread_create(&threadid[0], NULL, thread1, NULL);
+    sleep(1);
+    pthread_create(&threadid[1], NULL, thread2, NULL);
+    
+    pthread_join(threadid[0], NULL);
+    pthread_join(threadid[1], NULL);
+
+	// Execution will never get this far, so there is no reason to try closing.
+    printf("If you see this message, the program DID NOT deadlock.\n");
+	return 0;
+}
+
+/*
+ * +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+ * SUMMARY: thread1
+ * +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+*/
+void *thread1()
+{
+    // variable definitions
+	int fd;
+	char ch, write_buf[100], read_buf[10];
+	int offset, origin;
+	
+    // OPEN DEVICE A FIRST TIME
+	fd = open(DEVICE, O_RDWR);
+	if(fd == -1) 
+    {
+		printf("File %s either does not exist or has been locked by another "
+				"process\n", DEVICE);
+		exit(-1);
+	}
+    printf("T1: Opened device.\n");
+
+    sleep(2);
+    printf("T1: Switching to MODE 2\n");
+    ioctl(fd, E2_IOCMODE2);
+
+    // Shouldn't be able to get this far into the program
+    pthread_exit(NULL);
+}
+
+/*
+ * +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+ * SUMMARY: thread2
+ * +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+*/
+void *thread2()
+{
+    // variable definitions
+	int fd;
+	char ch, write_buf[100], read_buf[10];
+	int offset, origin;
+	
+    // OPEN DEVICE A FIRST TIME
+    printf("T2: Opening device.\n");
+	fd = open(DEVICE, O_RDWR);
+	if(fd == -1) 
+    {
+		printf("File %s either does not exist or has been locked by another "
+				"process\n", DEVICE);
+		exit(-1);
+	}
+
+    printf("T2: Device opened. This message should not display!!!\n");
+    pthread_exit(NULL);
 }
